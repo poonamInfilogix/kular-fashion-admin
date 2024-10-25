6f5952fca3e84e7bf64ec2ab063b234f0809e575
ashaInfilogix <ashainfilogix@gmail.com>
2024-10-25 12:07:17 +0530

    category listing added onError

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -3,7 +3,6 @@
 @section('content')
     <div class="page-content">
         <div class="container-fluid">
-
             <!-- start page title -->
             <div class="row">
                 <div class="col-12">
@@ -26,34 +25,36 @@
 
                     <div class="card">
                         <div class="card-body">
-                            <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+                            <table id="datatable" class="table table-bordered dt-responsive nowrap w-100">
                                 <thead>
                                     <tr>
                                         <th>#</th>
                                         <th>Name</th>
-                                        <th>Description</th>
+                                        <th>Image</th>
                                         <th>Status</th>
                                         <th>Action</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     @foreach($categories as $key => $category)
-                                    <tr>
-                                        <td>{{ ++$key }}</td>
-                                        <td>{{ $category->name }}</td>
-                                        <td><img src="{{ asset($category->image) }}" width="50" height="50"></td>
-                                        <td>
-                                            <input type="checkbox" id="{{ $category->id }}"  class="update-status" data-id="{{ $category->id }}" switch="success"  data-on="Active" data-off="Inactive" {{ $category->status === 'Active' ? 'checked' : '' }} data-endpoint="{{ route('update-status')}}"/>
-                                            <label for="{{ $category->id }}" data-on-label="Active" data-off-label="Inactive"></label>
-                                        </td>
-                                        <td class="action-buttons">
-                                            <a href="{{ route('categories.edit', $category->id)}}" class="btn btn-primary btn-sm edit"><i class="fas fa-pencil-alt"></i></a>
-                                            <button data-source="Category" data-endpoint="{{ route('categories.destroy', $category->id)}}"
-                                                class="delete-btn btn btn-danger btn-sm edit">
-                                                <i class="fas fa-trash-alt"></i>
-                                            </button>
-                                        </td>
-                                    </tr>
+                                        <tr>
+                                            <td>{{ ++$key }}</td>
+                                            <td>{{ $category->name }}</td>
+                                            <td><img src="{{ asset($category->image) }}" width="50" height="50" 
+                                                onerror="this.onerror=null; this.src='{{ asset(setting('default_category_image')) }}';" >
+                                            </td>
+                                            <td>
+                                                <input type="checkbox" id="{{ $category->id }}"  class="update-status" data-id="{{ $category->id }}" switch="success"  data-on="Active" data-off="Inactive" {{ $category->status === 'Active' ? 'checked' : '' }} data-endpoint="{{ route('update-status')}}"/>
+                                                <label for="{{ $category->id }}" data-on-label="Active" data-off-label="Inactive"></label>
+                                            </td>
+                                            <td class="action-buttons">
+                                                <a href="{{ route('categories.edit', $category->id)}}" class="btn btn-primary btn-sm edit"><i class="fas fa-pencil-alt"></i></a>
+                                                <button data-source="Category" data-endpoint="{{ route('categories.destroy', $category->id)}}"
+                                                    class="delete-btn btn btn-danger btn-sm edit">
+                                                    <i class="fas fa-trash-alt"></i>
+                                                </button>
+                                            </td>
+                                        </tr>
                                     @endforeach
                                 </tbody>
                             </table>
@@ -61,7 +62,6 @@
                     </div>
                 </div> <!-- end col -->
             </div> <!-- end row -->
-
         </div> <!-- container-fluid -->
     </div>
     <x-include-plugins :plugins="['dataTable', 'update-status', 'imagePreview' ]"></x-include-plugins>
