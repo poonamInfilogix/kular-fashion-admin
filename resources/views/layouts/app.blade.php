3f9a98f59c37571ebeea1bb8021def708e5c7fee
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2024-12-03 10:44:42 +0530

    Fixed sidebar scroll issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,7 @@
 
     <script src="{{ asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('assets/libs/metismenu/metisMenu.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
     <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
 
     <script src="{{ asset('assets/libs/moment/min/moment.min.js') }}"></script>
