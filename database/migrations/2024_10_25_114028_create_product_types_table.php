4c0d34bd3441a759a7fe9d764241b3c21160a571
satwinder <satwinderinfilogix@gmail.com>
2025-01-30 15:30:25 +0530

    product import export

--- database/migrations/2024_10_25_114028_create_product_types_table.php
+++ database/migrations/2024_10_25_114028_create_product_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product_types', function (Blueprint $table) {
             $table->id();
             $table->string('product_type_name');
-            $table->string('slug')->unique();
+            $table->string('slug')->nullable()->change()->unique();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
