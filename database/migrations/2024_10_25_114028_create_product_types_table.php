e18073259c210919169b5c85ff99274a5b8de29f
pummykular <harjesh@hotmail.com>
2025-02-10 11:19:49 +0000

    Fixed migration issue

--- database/migrations/2024_10_25_114028_create_product_types_table.php
+++ database/migrations/2024_10_25_114028_create_product_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product_types', function (Blueprint $table) {
             $table->id();
             $table->string('product_type_name');
-            $table->string('slug')->nullable()->change()->unique();
+            $table->string('slug')->nullable()->unique();
             $table->text('description')->nullable();
             $table->string('image')->nullable();
             $table->enum('status', ['Active', 'Inactive'])->default('Active');
