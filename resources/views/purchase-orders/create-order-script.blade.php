08749569fcdbe642beaaf73044afad85175116cb
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2025-02-11 17:28:45 +0530

    Updated purchase order module

--- resources/views/purchase-orders/create-order-script.blade.php
+++ resources/views/purchase-orders/create-order-script.blade.php
@@ -1,6 +1,5 @@
-
 @push('scripts')
-   <script>
+    <script>
         function addNewProduct(index, productInfo = null, errors = []) {
             let productCodeValue = productInfo?.product_code || '';
 
@@ -11,20 +10,8 @@
                             <x-form-input name="products[${index}][product_code]" value="${productCodeValue}" label="Product Code" placeholder="Enter Product Code" required />
                         </div>
                         <div class="col-sm-6 col-md-2 mb-3">
-                            <x-form-input name="products[${index}][supplier_color_code]" label="Supplier Color Code" placeholder="Enter Supplier Color Code" required />
-                        </div>
-                        <div class="col-sm-6 col-md-2 mb-3">
-                            <label for="color">Select Color <span class="text-danger">*</span></label>
-                            <select name="products[${index}][colors[]]" class="form-control">
-                                <option value="" disabled selected>Select Color</option> 
-                                @foreach ($colors as $color)
-                                    <option value="{{ $color->id }}"> {{ $color->color_name }} ({{ $color->color_code }})</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-sm-6 col-md-2 mb-3">
-                            <label for="product_type">Product Type<span class="text-danger">*</span></label>
-                            <select name="products[${index}][product_type]" class="form-control">
+                            <label for="product_type_${index}">Product Type<span class="text-danger">*</span></label>
+                            <select name="products[${index}][product_type]" class="form-control product-type-dropdown">
                                 <option value="" disabled selected>Select Product Type</option> 
                                 @foreach ($productTypes as $productType)
                                     <option value="{{ $productType->id }}">{{ $productType->product_type_name }}</option>
@@ -32,8 +19,8 @@
                             </select>
                         </div>
                         <div class="col-sm-6 col-md-2 mb-3">
-                            <label for="size_scale">Size Scale<span class="text-danger">*</span></label>
-                            <select name="products[${index}][size_scale]" id="size_scale" class="form-control size-scale-dropdown">
+                            <label for="size_scale-${index}">Size Scale<span class="text-danger">*</span></label>
+                            <select name="products[${index}][size_scale]" id="size_scale-${index}" class="form-control size-scale-dropdown">
                                 <option value="" disabled selected>Select size scale</option>
                                 @foreach ($sizeScales as $sizeScale)
                                     <option value="{{ $sizeScale->id }}" @selected(old('size_scale_id', $product->size_scale_id ?? '') == $sizeScale->id)>
@@ -63,14 +50,14 @@
                             <x-form-input name="products[${index}][delivery_date]" label="Delivery Date" class="date-picker" placeholder="Enter Delivery Date" required />
                         </div>
                         <div class="col-sm-6 col-md-2 mb-3">
-                            <x-form-input name="products[${index}][supplier_color_code]" label="Unit Price" placeholder="Enter Unit Price" required />
+                            <x-form-input name="products[${index}][supplier_color_code]" label="Price" placeholder="Enter Price" required />
                         </div>
                         <div class="col-sm-6 col-md-3 mb-3">
                             <x-form-input name="products[${index}][short_description]" label="Short Description" placeholder="Enter Short Description" required />
                         </div>
                         <div class="col-sm-6 col-md-3 mt-4">
-                            <button type="button" class="btn btn-primary add-product-variant"><i class="fas fa-plus"></i> Variant</button>
-                            <button type="button" class="btn btn-secondary copy-product"><i class="fas fa-copy"></i></button>
+                            <button type="button" class="btn btn-primary add-product-variant" disabled data-toggle="modal" data-target="#variantModal"><i class="fas fa-plus"></i> Variant</button>
+                            <button type="button" class="btn btn-secondary copy-product" disabled><i class="fas fa-copy"></i></button>
                             <button type="button" class="btn btn-danger remove-product-field"><i class="fas fa-trash-alt"></i></button>
                         </div>
                     </div>
@@ -78,6 +65,31 @@
                 </div>
             `;
             $('#product-fields-container').append(newField);
+
+            $('.product-type-dropdown').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Product Type'
+            });
+
+            $('.size-scale-dropdown').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Size Scale'
+            });
+
+            $('.size-scale-dropdown').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Size Scale'
+            });
+
+            $('.min-size-dropdown').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Min Size'
+            });
+
+            $('.max-size-dropdown').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Max Size'
+            });
         }
 
         $(function() {
@@ -86,9 +98,24 @@
                 placeholder_text_multiple: 'Select Supplier'
             });
 
+            $('#color').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Color'
+            });
+
             $('[name="products[0][product_type]"]').chosen({
                 width: '100%',
                 placeholder_text_multiple: 'Select Product Type'
+            });
+
+            $('[name="products[0][min_size]"]').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Min Size'
+            });
+
+            $('[name="products[0][max_size]"]').chosen({
+                width: '100%',
+                placeholder_text_multiple: 'Select Max Size'
             });
 
             $('[name="products[0][size_scale]"]').chosen({
@@ -98,11 +125,11 @@
         });
 
         $(document).ready(function() {
-            let oldProducts = @json(old('products', [])); 
-            if(oldProducts.length > 1){
+            let oldProducts = @json(old('products', []));
+            if (oldProducts.length > 1) {
                 let errors = @json($errors->messages());
 
-                for(let i=1; i<oldProducts.length; i++){
+                for (let i = 1; i < oldProducts.length; i++) {
                     addNewProduct(i, oldProducts[i], errors);
                 }
             }
@@ -157,5 +184,161 @@
                 }
             });
         });
+
+        function getSizes(productIndex) {
+            let minSize = $(`[name="products[${productIndex}][min_size]"]`).val();
+            let maxSize = $(`[name="products[${productIndex}][max_size]"]`).val();
+
+
+            let sizeRange = [];
+            for (let i = parseInt(minSize); i <= parseInt(maxSize); i++) {
+                let sizeOption = $(`[name="products[${productIndex}][min_size]"] option[value="${i}"]`);
+                let sizeLabel = sizeOption.html();
+                let sizeId = sizeOption.val();
+
+                sizeRange.push({
+                    id: sizeId,
+                    size: sizeLabel
+                });
+            }
+
+            return sizeRange;
+        }
+
+        function updateProductDependButtonsAttr(productIndex) {
+            let minSize = $(`[name="products[${productIndex}][min_size]"]`).val();
+            let maxSize = $(`[name="products[${productIndex}][max_size]"]`).val();
+
+            if (minSize && maxSize) {
+                let sizesColumn = ``;
+
+                getSizes(productIndex).forEach(function(sizeObj) {
+                    sizesColumn += `<th>${sizeObj.size}</th>`;
+                });
+
+                $(`[data-product-index="${productIndex}"] .variants-container`).html(`<table class="table table-sm">
+                    <thead>
+                        <tr>
+                            <th>Size</th>
+                            ${sizesColumn}
+                        </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>`);
+
+                $(`[data-product-index="${productIndex}"] .add-product-variant`).removeAttr('disabled');
+                $(`[data-product-index="${productIndex}"] .copy-product`).removeAttr('disabled');
+            } else {
+                $(`[data-product-index="${productIndex}"] .add-product-variant`).attr('disabled', 'disabled');
+                $(`[data-product-index="${productIndex}"] .copy-product`).attr('disabled', 'disabled');
+                $(`[data-product-index="${productIndex}"] .variants-container`).html(``);
+            }
+        }
+
+        $(function() {
+            $(document).on('change', '.min-size-dropdown', function() {
+                let productIndex = $(this).parents('[data-product-index]').attr('data-product-index');
+                updateProductDependButtonsAttr();
+
+                var minSizeId = parseInt($(this).val());
+
+                $(`[data-product-index="${productIndex}"] .max-size-dropdown`).prop('disabled', false)
+                    .trigger('chosen:updated');
+                $(`[data-product-index="${productIndex}"] .max-size-dropdown option`).show();
+
+                $(`[data-product-index="${productIndex}"] .max-size-dropdown option`).each(function() {
+                    var maxSizeId = parseInt($(this).val());
+
+                    if (maxSizeId < minSizeId) {
+                        $(this).hide();
+                    }
+                });
+
+                $(`[data-product-index="${productIndex}"] .max-size-dropdown`).trigger('chosen:updated');
+                var visibleOptions = $(
+                    `[data-product-index="${productIndex}"] .max-size-dropdown  option:visible`);
+
+                if (visibleOptions.length > 0) {
+                    var lastVisibleOptionValue = visibleOptions.last().val();
+                    $(`[data-product-index="${productIndex}"] .max-size-dropdown`).val(
+                        lastVisibleOptionValue).trigger('chosen:updated');
+                } else {
+                    var lastOptionValue = $('#size_range_max option:last').val();
+                    $(`[data-product-index="${productIndex}"] .max-size-dropdown`).val(lastOptionValue)
+                        .trigger('chosen:updated');
+                }
+            });
+
+            $(document).on('change', '.max-size-dropdown', function() {
+                updateProductDependButtonsAttr($(this).parents('[data-product-index]').attr(
+                    'data-product-index'));
+            });
+
+            $(document).on('click', '.add-product-variant', function() {
+                $('#selected_product_index').val($(this).parents('[data-product-index]').attr(
+                    'data-product-index'));
+            });
+
+            $(document).on('click', '.save-variant', function() {
+                let productIndex = $('#selected_product_index').val();
+                let supplierColorCode = $('#supplier_color_code').val();
+                let supplierColorName = $('#supplier_color_name').val();
+                let color = $('#color').val();
+
+                let minSize = $(`[name="products[${productIndex}][min_size]"]`).val();
+                let maxSize = $(`[name="products[${productIndex}][max_size]"]`).val();
+
+                let quantityInputCell = ``;
+                let variantIndex = $(`[data-product-index="${productIndex}"] .variants-container tbody tr`).length;
+
+                getSizes(productIndex).forEach(function(sizeObj) {
+                    quantityInputCell += `<td><input type="text" name="products[${productIndex}][variants][${variantIndex}][size][${sizeObj.id}]"></td>`;
+                });
+
+                $(`[data-product-index="${productIndex}"] .variants-container tbody`).append(`<tr>
+                    <td>Red</td>
+                    ${quantityInputCell}
+                </tr>`);
+            });
+        })
     </script>
 @endpush
+
+
+<div class="modal fade" id="variantModal" tabindex="-1" role="dialog" aria-labelledby="variantModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addVariantModalLabel">Add New Variant</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <input type="hidden" id="selected_product_index">
+                <div class="mb-3">
+                    <x-form-input name="supplier_color_code" label="Supplier Color Code"
+                        placeholder="Enter Supplier Color Code" required />
+                </div>
+                <div class="mb-3">
+                    <x-form-input name="supplier_color_name" label="Supplier Color Name"
+                        placeholder="Enter Supplier Color Name" required />
+                </div>
+                <div class="mb-3">
+                    <label for="color" class="form-label">Select Color <span class="text-danger">*</span></label>
+                    <select id="color" name="color" class="form-control" required>
+                        <option value="" disabled selected>Select Color</option>
+                        @foreach ($colors as $color)
+                            <option value="{{ $color->id }}">{{ $color->color_name }}
+                                ({{ $color->color_code }})
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary save-variant">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
