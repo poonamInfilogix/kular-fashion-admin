07333ac1333d64147a02ef1271f3e49380c5f5ba
Satpal Singh Sekhon <satpalinfilogix@gmail.com>
2025-02-20 13:13:23 +0530

    Moved general settings in header

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,16 +1,10 @@
 @extends('layouts.app')
 
+@section('title', 'Dashboard')
+
 @section('content')
     <div class="page-content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Dashboard</h4>
-                    </div>
-                </div>
-            </div>
-
            @include('dashboard.components.widgets')
         </div>
     </div>
--- resources/views/settings/web-settings/index.blade.php
+++ resources/views/settings/web-settings/index.blade.php
@@ -12,56 +12,66 @@
 
                     <div class="card">
                         <div class="card-body">
-                            <form class="form-horizontal" action="{{ route('web-settings.store') }}" method="POST" enctype="multipart/form-data">
+                            <form class="form-horizontal" action="{{ route('web-settings.store') }}" method="POST"
+                                enctype="multipart/form-data">
                                 @csrf
                                 <div class="row">
                                     <div class="col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                         <div class="mb-3">
-                                            <x-form-input type="file" name="web_icon" id="" class="form-control" label="Choose icon" />
-                                                
-                                                    <div class="row">
-                                                        <div class="col-md-6">
-                                                            @if (setting('web_icon'))
-                                                                <img src="{{ asset(setting('web_icon')) }}" id="preview-web_icon"
-                                                                class="img-preview img-fluid mt-2">
-                                                                @else
-                                                                <img src="" id="preview-web_icon" class="img-fluid mt-2 w-150" hidden>
-                                                            @endif
-                                                            
-                                                        </div>
-                                                    </div>
+                                            <x-form-input type="file" name="web_icon" id="" class="form-control"
+                                                label="Choose icon" />
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    @if (setting('web_icon'))
+                                                        <img src="{{ asset(setting('web_icon')) }}" id="preview-web_icon"
+                                                            class="img-preview img-fluid mt-2">
+                                                    @else
+                                                        <img src="" id="preview-web_icon"
+                                                            class="img-fluid mt-2 w-150" hidden>
+                                                    @endif
+
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                         <div class="mb-3">
-                                            <x-form-input type="file" name="web_favicon" id="web_favicon" class="form-control" label="Choose favicon" />
-                                                
-                                                    <div class="row">
-                                                        <div class="col-md-6">
-                                                            @if (setting('web_favicon'))
-                                                                <img src="{{ asset(setting('web_favicon')) }}" id="preview-web_favicon"
-                                                                class="img-preview img-fluid mt-2">
-                                                                @else
-                                                                <img src="" id="preview-web_favicon" class="img-fluid mt-2 w-150" hidden>
-                                                            @endif
-                                                            
-                                                        </div>
-                                                    </div>
+                                            <x-form-input type="file" name="web_favicon" id="web_favicon"
+                                                class="form-control" label="Choose favicon" />
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    @if (setting('web_favicon'))
+                                                        <img src="{{ asset(setting('web_favicon')) }}"
+                                                            id="preview-web_favicon" class="img-preview img-fluid mt-2">
+                                                    @else
+                                                        <img src="" id="preview-web_favicon"
+                                                            class="img-fluid mt-2 w-150" hidden>
+                                                    @endif
+
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                         <div class="mb-3">
-                                            <x-form-input name="web_site_title" value="{{ setting('web_site_title') }}" label="web site title" placeholder="Enter web sit title" required="true" />
+                                            <x-form-input name="web_site_title" value="{{ setting('web_site_title') }}"
+                                                label="web site title" placeholder="Enter web sit title" required="true" />
                                         </div>
                                     </div>
                                     <div class="col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                         <div class="mb-3">
-                                            <x-form-input name="web_contact_no" value="{{ setting('web_contact_no') }}" label="web contact number" placeholder="Enter web contact number" required="true" />
+                                            <x-form-input name="web_contact_no" value="{{ setting('web_contact_no') }}"
+                                                label="web contact number" placeholder="Enter web contact number"
+                                                required="true" />
                                         </div>
                                     </div>
                                     <div class="col-sm-6 col-md-4 col-lg-4 col-xl-3">
                                         <div class="mb-3">
-                                            <x-form-input name="web_contact_email" value="{{ setting('web_contact_email') }}" label="web contact email" placeholder="Enter web contact email" required="true" />
+                                            <x-form-input name="web_contact_email"
+                                                value="{{ setting('web_contact_email') }}" label="web contact email"
+                                                placeholder="Enter web contact email" required="true" />
                                         </div>
                                     </div>
                                 </div>
