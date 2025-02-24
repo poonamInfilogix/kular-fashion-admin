2e934cbec8a54fd8018a661e1642c7c9a9b3ff25
Aman Aharwal <amaninfilogix@gmail.com>
2025-02-24 13:41:08 +0530

    fix tittle  and back button issue in   edit branch

--- resources/views/branches/edit.blade.php
+++ resources/views/branches/edit.blade.php
@@ -8,20 +8,6 @@
 @section('content')
     <div class="page-content">
         <div class="container-fluid">
-
-            <!-- start page title -->
-            <div class="row">
-                <div class="col-12">
-                    <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0 font-size-18">Update Branch</h4>
-                        <div class="page-title-right">
-                            <a href="{{ route('branches.index') }}" class="btn btn-primary">Back</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- end page title -->
-
             <div class="row">
                 <div class="col-12">
                     <x-error-message :message="session('message')" />
