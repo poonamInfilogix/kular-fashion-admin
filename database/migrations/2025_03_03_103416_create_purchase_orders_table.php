16a513831b16be4f3f56c1be4fd4b7dfaa876ad0
ashaInfilogix <ashainfilogix@gmail.com>
2025-03-03 18:18:40 +0530

    products created, migration.

---
+++ database/migrations/2025_03_03_103416_create_purchase_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchase_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('supplier_id');
+            $table->string('order_no');
+            $table->date('supplier_order_date');
+            $table->date('delivery_date')->nullable();
+
+            $table->timestamps();
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchase_orders');
+    }
+};
